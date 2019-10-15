b78541ad290b0adf1e3f25d145ef842718aa929d
HeshSs <hishmat.salehi2015@gmail.com>
2019-10-15 00:44:59 -0400

    Got the algorithm and solved 90% of the problem

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-import java.util.Stack;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String question = "(12@45)&14";
-        String st = "(12@45)";
-        String ab = question.replace(st, String.valueOf(maxOrMinCalculator(st)));
-        System.out.println(ab);
-        System.out.println(maxOrMinCalculator(st));
-        System.out.println(maxOrMinCalculator("(02)"));
+        Scanner scanner = new Scanner(System.in);
+        String question = scanner.next();
+        question = "(" + question + ")";
+        answerGenerator(question);
+        System.out.println(answerGenerator(question));
 
     }
 
@@ -28,40 +27,47 @@ public class Main {
         }
 
         if (operation.equals("@")) {
-            return Math.min(a, b);
+            return Integer.parseInt(String.valueOf(Math.min(a, b)));
         } else if (operation.equals("&")) {
-            return Math.max(a, b);
+            return Integer.parseInt(String.valueOf(Math.max(a, b)));
         } else if (operation == "" && a == 0 && b == 0) {
             return Integer.parseInt(string.substring(1,string.length()-1));
         } else {
             throw (new Exception("Error")); // Change this to an exception
         }
     }
-    static void answerGenerator(String input) {
+    static String answerGenerator(String input) throws Exception {
         int openBracketIndex = 0;
         int closedBracketIndex = 0;
 
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '(') {
+            if (input.charAt(i) == '(' ) {
                 openBracketIndex = i;
             } else if (input.charAt(i) == ')' ) {
                 closedBracketIndex = i;
+                break;
             }
         }
+
+        if (input.contains("(") == false) {
+            return input;
+        }
+        String withinBrackets = input.substring(openBracketIndex, closedBracketIndex+1);
+        System.out.println(withinBrackets);
+
+        input = input.replace(withinBrackets, String.valueOf(maxOrMinCalculator(withinBrackets)));
+
+        return answerGenerator(input);
+        //answerGenerator(input.replace());
+
     }
 }
+
 /*
-    static int recursiveSolution(String input) {
-        Integer a = 0;
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '@' || input.charAt(i) == '&') {
-                try {
-                    a = Integer.parseInt(input.substring(i-1,i));
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return (Integer) a;
-    }
+        String st = "(12@45)";
+        String ab = question.replace(st, String.valueOf(maxOrMinCalculator(st)));
+        System.out.println(ab);
+        System.out.println(maxOrMinCalculator(st));
+        System.out.println(maxOrMinCalculator("(02)"));
+
  */
