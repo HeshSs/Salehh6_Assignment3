64f1741354b57b6fa79e8f03d045111ec6a8bad9
HeshSs <hishmat.salehi2015@gmail.com>
2019-10-17 17:48:37 -0400

    Comments added to the assignment and it's ready for submission

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Asks for user input and adds brackets to it
         Scanner scanner = new Scanner(System.in);
         String question = scanner.nextLine();
         question = "(" + question + ")";
 
+        // Using Try-Catch block to output the final answer
         try {
-            System.out.println(answerGenerator(question));
+            System.out.println(answerGenerator(question)); // Try to output the answer
         } catch (NumberFormatException e) {
-            System.out.println("INVALID EXPRESSION");
+            System.out.println("INVALID EXPRESSION"); // If NumberFormat Exception was thrown printout "INVALID EXPRESSION"
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()); /* Any other Exception Just printout the message of the Exception
+                                                   e.g. if invalidC Exception was thrown, print "INVALID CHARACTERS"  */
         }
 
     }
 
+    /***
+     *
+     * @param string Given expression (e.g. (a&b) or (a@b) )
+     * @return Max of a or b if (&) is the operator or Min, if (@) is the operator
+     */
     private static Integer maxOrMinCalculator(String string){
 
         String operation = "";
@@ -27,11 +35,9 @@ public class Main {
 
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) == '@' || string.charAt(i) == '&') {
-
                 operation = String.valueOf(string.charAt(i));
                 a = Integer.parseInt(string.substring(1, i));
                 b = Integer.parseInt(string.substring(i + 1, string.length() - 1));
-
             }
         }
             if (operation.equals("@")) {
@@ -45,12 +51,18 @@ public class Main {
             }
     }
 
+    /***
+     *
+     * @param input Can be any expression valid {e.g. 22@((12@45)&14)} or invalid {e.g. (23&(-25))}
+     * @return An Integer or an Exception
+     * @throws Exception throws invalidC or invalidE Exceptions
+     */
     private static String answerGenerator(String input) throws Exception {
 
-        Exception invalidC = new Exception("INVALID CHARACTERS");
-        Exception invalidE = new Exception("INVALID EXPRESSION");
-        String invalidCharacters = " -~!#$%^*_+qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-        String validCharacters = ")0123456789";
+        Exception invalidC = new Exception("INVALID CHARACTERS");       // invalidC Exception for cases where the input has invalid characters
+        Exception invalidE = new Exception("INVALID EXPRESSION");       // invalidE Exception for cases where the input is a invalid expression
+        String invalidCharacters = " -~!#$%^*_+qwertyuiop[]asdfghjkl;'zxcvbnm,./";  // list of invalid characters
+        String validCharacters = ")0123456789";                         // most of the valid characters for expression checking
         Stack<Character> stack = new Stack<>();
         int openBracketIndex = 0;
         int closedBracketIndex = 0;
@@ -59,12 +71,12 @@ public class Main {
         for (int i = 0; i < input.length(); i++) {
             for (int j = 0; j < invalidCharacters.length(); j++) {
                 if (input.charAt(i) == invalidCharacters.charAt(j)) {
-                    throw invalidC; // "INVALID CHARACTERS"
+                    throw invalidC;         // "INVALID CHARACTERS"
                 }
             }
         }
 
-        // Mismatching Parentheses
+        // When there are mismatching parentheses throw invalidE Exception
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '(' )  {
                 stack.push(input.charAt(i));
@@ -72,27 +84,27 @@ public class Main {
                 try {
                     stack.pop();
                 } catch (Exception e) {
-                    throw invalidE; // "INVALID EXPRESSION"
+                    throw invalidE;
                 }
             }
         }
 
         if (!stack.empty()) {
-            throw invalidE; // "INVALID EXPRESSION"
+            throw invalidE;
         }
 
-        // (45(34)) or 43(44) or )(
+        // For cases like (45(34)) or 43(44) or )( throw invalidE Exception
         for (int i = 1; i < input.length(); i++) {
             if (input.charAt(i) == '(') {
                 for (int j = 0; j < validCharacters.length(); j++) {
                     if (input.charAt(i-1) == validCharacters.charAt(j)) {
-                        throw invalidE; // "INVALID EXPRESSION"
+                        throw invalidE; //
                     }
                 }
             }
         }
 
-        // Cases 25&30& or (&25&30)
+        // For cases like 25&30& or (&25&30) throw invalidE Exception
         for (int i = 1; i < input.length(); i++) {
             if (input.charAt(i) == '&' || input.charAt(i) == '@') {
                 if (input.charAt(i-1) == '(' || input.charAt(i+1) == ')' ) {
@@ -101,6 +113,7 @@ public class Main {
             }
         }
 
+        // Find indices of substring of inside an open and closed bracket
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '(' ) {
                 openBracketIndex = i;
@@ -110,14 +123,17 @@ public class Main {
             }
         }
 
+        // Base case of the Recursion. If there is no more brackets '(' in the string return the answer
         if (!input.contains("(")) {
             return input;
         }
 
+        // Create a within brackets substring that starts with '(' and ends with ')'
         String withinBrackets = input.substring(openBracketIndex, closedBracketIndex+1);
 
         int counter = 0;
 
+        // If substring has more than one @ or &
         for (int i = 0; i < withinBrackets.length(); i++) {
             if (withinBrackets.charAt(i) == '@' || withinBrackets.charAt(i) == '&') {
                 counter ++;
@@ -125,31 +141,40 @@ public class Main {
         }
 
         if (counter > 1) {
-
-            input = input.replace(withinBrackets, parenthesesAdder(withinBrackets));
+            input = input.replace(withinBrackets, parenthesesAdder(withinBrackets)); // Add brackets to it
             try {
-                return answerGenerator(input);
+                return answerGenerator(input); // And then return the answer
             } catch (Exception e) {
-                throw invalidC;
+                throw invalidC; // Else throw invalidC Exception
             }
 
         }
 
+        // If every other cases was satisfied, then replace the within brackets with its max or min answer
         input = input.replace(withinBrackets, String.valueOf(maxOrMinCalculator(withinBrackets)));
-        try {
-            return answerGenerator(input);
-        } catch (Exception e) {
-            throw invalidC;
-        }
 
+        try {
+            return answerGenerator(input); // And then return the answer
+        } catch (Exception e) {
+            throw invalidC; // Else throw invalidC Exception
+        }
     }
+
+    /***
+     *
+     * @param str When str has more than one @ or &
+     * @return Adds brackets to the str from left to right
+     */
     private static String parenthesesAdder(String str) {
+
         int count = 0;
+
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '&' || str.charAt(i) == '@' ) {
                 count ++;
             }
         }
+
         for (int i = 0; i < count; i++) {
             str = "(" + str;
         }
@@ -157,33 +182,6 @@ public class Main {
         str = str.replaceAll("&", ")&");
         str = str.replaceAll("@", ")@");
         return str;
+
     }
 }
-
-/*
-Test Cases
-------------
-Part1
-------------
-test1 = (((22@(55&(55)))&(33&44))&((40@23)&(50@25))) == 44
-test2 = (((22@(55&(55)))&(33&44))&((40@23)&(50@25)))@(3&2&33@66) == 33
-test3 = 22@((12@45)&14) == 14
-test4 = (23&24@23&24&25&(23@24))&(23&24&22&21) == 25
-test5 = (23333) == 23333
-------------
-Part2
-------------
-test1 = (((22@(55&55))&(33&44))&(((40@23)&(50@25))) == "INVALID EXPRESSION"
-test2 = (22@((12@45)&14)) ( == "INVALID CHARACTERS"
-test3 = (22@((12@45)&14))( == "INVALID EXPRESSION"
-test4 = (22+((12@45)14z)) == "INVALID CHARACTERS"
-test5 = (86&45@23&45&84)@(45((23))) ==  "INVALID EXPRESSION"
-test6 = (88&(23@12))(76) == "INVALID EXPRESSION"
-test7 = (((((98#))))))) == "INVALID CHARACTERS"
-test8 = (23&(-25)) == "INVALID CHARACTERS"
-test9 = 25&30& == "INVALID EXPRESSION"
-test10 = &27@(19) == "INVALID EXPRESSION"
-test11 = (2#5) == "INVALID CHARACTERS"
-
-------------
- */
